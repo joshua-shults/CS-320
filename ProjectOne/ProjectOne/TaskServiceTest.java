5bf2ecbaf363a70a8d691555e2917eb0824611d8
joshua-shults <148671392+joshua-shults@users.noreply.github.com>
2024-12-22 11:54:05 -0500

    Add files via upload

---
+++ ProjectOne/ProjectOne/TaskServiceTest.java
@@ -0,0 +1,44 @@
+package taskserviceapp;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class TaskServiceTest {
+	 @Test
+	    void testAddTask() {
+	        TaskService service = new TaskService();
+	        String taskId = service.addTask("Task Name", "Task Description");
+
+	        Task task = service.getTask(taskId);
+	        assertNotNull(task);
+	        assertEquals("Task Name", task.getName());
+	        assertEquals("Task Description", task.getDescription());
+	    }
+
+	    @Test
+	    void testDeleteTask() {
+	        TaskService service = new TaskService();
+	        String taskId = service.addTask("Task Name", "Task Description");
+
+	        service.deleteTask(taskId);
+	        assertNull(service.getTask(taskId));
+	    }
+
+	    @Test
+	    void testUpdateTaskName() {
+	        TaskService service = new TaskService();
+	        String taskId = service.addTask("Old Name", "Description");
+
+	        service.updateTaskName(taskId, "New Name");
+	        assertEquals("New Name", service.getTask(taskId).getName());
+	    }
+
+	    @Test
+	    void testUpdateTaskDescription() {
+	        TaskService service = new TaskService();
+	        String taskId = service.addTask("Name", "Old Description");
+
+	        service.updateTaskDescription(taskId, "New Description");
+	        assertEquals("New Description", service.getTask(taskId).getDescription());
+	    }
+	
+
+}
